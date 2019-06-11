a55f28366086027b74d535a487ad90c04bbb1596
Karolina Pietruszka <pietruszka.karolina94@gmail.com>
2019-06-11 21:44:18 +0200

    task finished

---
+++ js/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const url = 'http://api.icndb.com/jokes/random';
+const paragraph = document.getElementById('joke');
+const button = document.getElementById('get-joke');
+
+button.addEventListener('click', function () {
+  getJoke();
+});
+
+function getJoke() {
+  let xhr = new XMLHttpRequest();
+  button.disabled = true;
+
+  xhr.open('GET', url);
+
+  xhr.addEventListener('load', function () {
+    const response = JSON.parse(xhr.response);
+    paragraph.innerHTML = response.value.joke;
+    button.disabled = false;
+  });
+  xhr.send();
+}
+
+getJoke();
